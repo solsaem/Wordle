4115ba0cdca41d3627e533b98d628c8a9f2ac1d9
dorajohanns <89972552+dorajohanns@users.noreply.github.com>
2022-03-29 20:53:49 +0000

    laga

--- PA6/wordle.py
+++ PA6/wordle.py
@@ -9,9 +9,9 @@ class Player:
 
 class Wordle:
     def __init__(self):
-        self.word = self.random_word()
+        self.word = self.random_word().lower()
         self.guesses = []
-        self.progress = []
+        self.feedback = []
         self.score = 0
 
     def random_word(self):
@@ -21,8 +21,11 @@ class Wordle:
         random_number = random.randint(1,212)
         return list_of_words[random_number]
 
-    def add(self, guess):
+    def add_guess(self, guess):
         self.guesses.append(guess)
+    
+    def add_feedback(self, feedback):
+        self.feedback.append(feedback)
     
     def __str__(self):
         pass
@@ -30,6 +33,7 @@ class Wordle:
 #hello
 def game(player):
     current_game = Wordle()
+    print(current_game.word)
     for i in range(0,5):
         guess = input("Enter guess number: ").lower()
         is_valid = check_guess(guess)
@@ -37,12 +41,24 @@ def game(player):
             print("Guess is not valid try again")
             guess = input("Enter guess number: ").lower()
             is_valid = check_guess(guess)
-        current_game.add(guess)
-        guess_feedback = check_letters(guess)
+        current_game.add_guess(guess)
+        guess_feedback = check_letters(current_game, current_game.word, guess)
+        current_game.add_feedback(guess_feedback)
+    print(current_game.guesses)
+    print(current_game.feedback)
 
 
-def check_letters(guess):
-    pass
+def check_letters(current_game, word ,guess):
+    my_str = ""
+    for i in range(0,5):
+        if guess[i] == word[i]:
+            my_str += "C"
+        elif guess[i] in word:
+            my_str += "c"
+        else:
+            my_str += "-"
+    return my_str
+
 
 
 def check_guess(guess):
