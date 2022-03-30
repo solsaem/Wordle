83b1490eefe66777d87907210102a0cb699d95b8
dorajohanns <89972552+dorajohanns@users.noreply.github.com>
2022-03-30 09:17:53 +0000

    Update wordle.py
    setja inn gamla

--- PA6/wordle.py
+++ PA6/wordle.py
@@ -6,23 +6,34 @@ class Player:
         self.losses = 0
         self.played = 0
         self.highscore = []
+    
+    def add_game(self, game, won_or_lost):
+        self.played += 1
+        self.highscore.append(game)
+        if won_or_lost == True:
+            self.wins += 1
+        else:
+            self.losses += 1
 
 class Wordle:
     def __init__(self):
-        self.word = self.random_word()
+        self.word = self.random_word().lower()
         self.guesses = []
-        self.progress = []
+        self.feedback = []
         self.score = 0
 
     def random_word(self):
-        file_object = open("/Users/halldorajohannsdottir/Desktop/HR 2022 vor/Gagnaskipan/PA6/word_bank.txt", 'r')
+        file_object = open("/Users/halldorajohannsdottir/Documents/GitHub/Wordle/PA6/word_bank.txt", 'r')
         read_file = file_object.read()
         list_of_words = read_file.split()
         random_number = random.randint(1,212)
         return list_of_words[random_number]
 
-    def add(self, guess):
+    def add_guess(self, guess):
         self.guesses.append(guess)
+    
+    def add_feedback(self, feedback):
+        self.feedback.append(feedback)
     
     def __str__(self):
         pass
@@ -30,6 +41,7 @@ class Wordle:
 
 def game(player):
     current_game = Wordle()
+    print(current_game.word)
     for i in range(0,5):
         guess = input("Enter guess number: ").lower()
         is_valid = check_guess(guess)
@@ -37,12 +49,31 @@ def game(player):
             print("Guess is not valid try again")
             guess = input("Enter guess number: ").lower()
             is_valid = check_guess(guess)
-        current_game.add(guess)
-        guess_feedback = check_letters(guess)
+        current_game.score += 1
+        current_game.add_guess(guess)
+        guess_feedback = check_letters(current_game, current_game.word, guess)
+        current_game.add_feedback(guess_feedback)
+        print(f'\n{guess} {guess_feedback}\n')
+        if guess_feedback == "CCCCC":
+            print("You found the word")
+            return current_game, True
+    print(f"The word was {current_game.word}")
+    return current_game, False
 
 
-def check_letters(guess):
-    pass
+def check_letters(current_game, word ,guess):
+    my_str = ""
+    for i in range(0,5):
+        if guess[i] == word[i]:
+            my_str += "C"
+        elif guess[i] in word:
+            my_str += "c"
+        else:
+            my_str += "-"
+    return my_str
+
+#test hihi
+    
 
 
 def check_guess(guess):
@@ -52,6 +83,7 @@ def check_guess(guess):
 
 def main():
     current_player = Player()
-    game(current_player)
+    this_game, win_or_loss = game(current_player)
+    current_player.add_game(this_game, win_or_loss)
 
 main()
