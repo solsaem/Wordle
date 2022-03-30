e3f08d374b7d3aaf80bd1df1b022da9d4397fc25
solsaem <sol21@ru.is>
2022-03-30 18:23:19 +0000

    nyir files

---
+++ PA6/wordbank.py
@@ -0,0 +1,13 @@
+class WordBank:
+    def __init__(self):
+        self.word_list = open_file()
+
+    def add_word(self, word):
+        if word not in self.word_list:
+            self.word_list.append(word + '\n')
+
+def open_file():
+    #file_object = open("/Users/sol/Documents/GitHub/Wordle/PA6/word_bank.txt", 'r')
+    file_object = open("/Users/halldorajohannsdottir/Documents/GitHub/Wordle/PA6/word_bank.txt", 'r')
+    read_file = file_object.read()
+    return read_file.split()
